6ef82b26c8ef8b4571430e6525422690c613faec
CPPaleyron <christophe.paleyron@hegele.de>
2025-07-29 10:03:10 +0200

    Create test example (will fail)

---
+++ Unit_test.py
@@ -0,0 +1,22 @@
+import unittest
+
+class mockObj():
+    def __init__(self, name:str, age:int):
+        self.name = name
+        self.age = age
+
+    def __eq__(self, other):
+        return isinstance(other, mockObj) and self.name == other.name and self.age == other.age
+
+
+class ExampleTestCases(unittest.TestCase):
+    def test_str(self):
+        self.assertEqual("string is the same", "string is the same", "strs are not equal")
+
+    def test_object(self):
+        obj1 = mockObj("Chris", 27)
+        obj2 = mockObj("Lukas", 28)
+        self.assertEqual(obj1, obj2, "Given Objects are not equal")
+
+if __name__ == '__main__':
+    unittest.main()
